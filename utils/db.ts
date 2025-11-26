9343653687a911151e3b6e08126f5bd90148b4cb
wing5237 <156151269+wing5237@users.noreply.github.com>
2025-11-26 15:17:23 +0800

    Update Gemini modal to version 3.0 Pro

--- utils/db.ts
+++ utils/db.ts
@@ -71,8 +71,8 @@ export type Settings = typeof initialSettings;
 
 export const uniModals: Model[] = [
     {
-        id: 'gemini-2.5-pro',
-        name: 'Gemini 2.5 Pro',
+        id: 'gemini-3-pro-preview',
+        name: 'Gemini 3.0 Pro',
         provider: 'google',
         type: 'universal'
     },
